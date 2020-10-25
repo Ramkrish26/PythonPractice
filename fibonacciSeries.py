7df8d360cea1123d43e6ccc03211841c48fc0b7b
Ram <ramakrishnan2629@gmail.com>
2020-10-25 13:40:01 +0530

    Added Fibonacci series program

---
+++ fibonacciSeries.py
@@ -0,0 +1,18 @@
+# To generate the fibinacci series
+
+num = input("Enter the number: ")
+num1 = 0
+num2 = 1
+
+for i in range(0, int(num)):
+    if(i == 0):
+        print(num1)
+    elif(i==1):
+        print(num2)
+    else:
+        num3 = num1 + num2
+        print(str(num3))
+        num1 = num2
+        num2 = num3
+
+
